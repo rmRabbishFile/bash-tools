66010e00e7b80f8ac5b448c73338a315df8e0472
xiang.zhang <xiang.zhang@autoflight.com>
2024-07-01 18:27:29 +0800

    add walid time for serives

--- open_todo.sh
+++ open_todo.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-firefox https://app.todoist.com/app/
-firefox https://calendar.google.com/calendar/u/0/r/day
+# Get the current hour in 24-hour format
+current_hour=$(date +%H)
+
+# Check if the current hour is between 20:00 and 6:00
+if [ "$current_hour" -ge 20 ] || [ "$current_hour" -lt 6 ]; then
+    echo "Script execution is skipped between 20:00 and 6:00"
+else
+    firefox https://app.todoist.com/app/
+    firefox https://calendar.google.com/calendar/u/0/r/day
+fi
 
 # sudo loginctl enable-linger <your_username>
 # systemctl --user enable open_webpage.timer
