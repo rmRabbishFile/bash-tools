d889968bad5d2412a6aae3156bf8f5d8d21893f8
xiang.zhang <xiang.zhang@autoflight.com>
2024-07-01 18:18:58 +0800

    add periodical open todo_page

---
+++ open_todo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+firefox https://app.todoist.com/app/
+firefox https://calendar.google.com/calendar/u/0/r/day
+
+# sudo loginctl enable-linger <your_username>
+# systemctl --user enable open_webpage.timer
+# systemctl --user start open_webpage.timer
+# sudo loginctl show-user <your_username> --property=Linger
+# systemctl --user status open_webpage.timer
+
